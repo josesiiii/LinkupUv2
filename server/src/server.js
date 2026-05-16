2bc5e90cf345147f09965c7b04c5b3358bad9555
JuanAguirreCastro <juanc.aguirre@pascualbravo.edu.co>
2026-05-16 12:51:07 -0500

    feat: integrate socket.io realtime infrastructure

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,9 @@
 import dotenv from "dotenv";
 
+import http from "http";
+
+import { Server } from "socket.io";
+
 import app from "./app.js";
 
 import connectDB from "./config/db.js";
@@ -12,12 +16,9 @@ dotenv.config();
 
 connectDB();
 
-const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en puerto ${PORT}`);
-});
 
+// ROUTES
 app.use(
   "/api/messages",
   messageRoutes
@@ -27,3 +28,110 @@ app.use(
   "/api/savedprofiles",
   savedProfileRoutes
 );
+
+
+
+// HTTP SERVER
+const server = http.createServer(app);
+
+
+
+// SOCKET.IO
+const io = new Server(server, {
+  cors: {
+    origin: "*"
+  }
+});
+
+
+
+// USUARIOS ONLINE
+const onlineUsers = {};
+
+
+
+// CONEXIÓN SOCKET
+io.on("connection", (socket) => {
+
+  console.log(
+    "Usuario conectado:",
+    socket.id
+  );
+
+
+
+  // REGISTRAR USUARIO
+  socket.on("registerUser", (userId) => {
+
+    onlineUsers[userId] = socket.id;
+
+    console.log("Usuarios online:");
+    console.log(onlineUsers);
+
+  });
+
+
+
+  // UNIRSE A ROOM
+  socket.on("joinRoom", (roomId) => {
+
+    socket.join(roomId);
+
+    console.log(
+      `Socket ${socket.id} unido a room ${roomId}`
+    );
+
+  });
+
+
+
+  // ENVIAR MENSAJE REALTIME
+  socket.on("sendMessage", (data) => {
+
+    io.to(data.roomId).emit(
+      "receiveMessage",
+      data
+    );
+
+  });
+
+
+
+  // DESCONECTAR
+  socket.on("disconnect", () => {
+
+    console.log(
+      "Usuario desconectado:",
+      socket.id
+    );
+
+    // eliminar usuario online
+    for (const userId in onlineUsers) {
+
+      if (
+        onlineUsers[userId] === socket.id
+      ) {
+
+        delete onlineUsers[userId];
+
+      }
+
+    }
+
+  });
+
+});
+
+
+
+const PORT = process.env.PORT || 5000;
+
+
+
+server.listen(PORT, () => {
+
+  console.log(
+    `Servidor corriendo en puerto ${PORT}`
+  );
+
+});
